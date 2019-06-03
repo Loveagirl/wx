835c0e2dc8c2a8bfe8a9f0b3a5c57b0612c4e12a
Nick <nick.ge@astasys.com>
2019-06-03 16:02:57 +0800

    two

---
+++ components/classic/movie/movie.js
@@ -0,0 +1,24 @@
+// components/classic/movie/movie.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    imgSrc:String,
+    text:String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    movie:"./images/movie@tag.png"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ components/date/index.js
@@ -0,0 +1,47 @@
+// components/date/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    num:{
+      type:Number,
+      observer:function(newVal,oldVal){
+        let val = newVal <10 ? "0"+newVal:newVal
+        console.log(val)
+        this.setData({
+          _index:val
+        })
+      }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    months:[
+      '一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月',
+      '十二月'
+    ],
+    year:Number,
+    month:String,
+    _index:String
+  },
+   
+    ready: function() {
+      var date=new Date();
+      var year=date.getFullYear()
+      var month = date.getMonth()
+      this.setData({
+        year:year,
+        month:this.data.months[month]
+      })
+    },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
